aeedf9ddcd9856ffc105d63e3ff4702b5ea9d041
Giovanni <giovannidwidjosewojo@hotmail.com>
2018-11-30 14:08:06 +0100

    Initial commit

---
+++ app/scripts/slick/islick.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+  $('.hints-content').slick({
+dots: true,
+prevArrow: false,
+    nextArrow: false,
+    infinite:false,
+    mobileFirst:true,
+
+  });
+});
+$(document).ready(function(){
+  $('.discover-content').slick({
+dots: true,
+prevArrow: false,
+    nextArrow: false,
+    infinite:false,
+    mobileFirst:true,
+
+  });
+});
