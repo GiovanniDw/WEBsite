a532c7a0efaa90c9429b6d29812abd6174e71c88
Giovanni <giovannidwidjosewojo@hotmail.com>
2019-01-31 13:45:52 +0100

    VERHAAL DINGEN

---
+++ js/verhaal.js
@@ -0,0 +1,117 @@
+
+function positionScroll() {
+  var element = document.querySelector('.verhaal section h1');
+  var desiredPosition = 50;
+
+
+  if (window.pageYOffset > desiredPosition) {
+    element.classList.add('onScroll')
+  } else {
+    element.classList.remove('onScroll')
+  }
+}
+window.onscroll = positionScroll;
+
+
+// console.log('hallo');
+//
+// var filterknop = document.querySelector('.filters');
+//
+// var formEl = document.querySelector('aside > form');
+//
+// filterknop.addEventListener('click', function(){
+//   formEl.classList.toggle('show-filters');
+// });
+
+//
+// var starClicked = false;
+//
+// $(function() {
+//
+//   $('.star').click(function() {
+//
+//     $(this).children('.selected').addClass('is-animated');
+//     $(this).children('.selected').addClass('pulse');
+//
+//     var target = this;
+//
+//     setTimeout(function() {
+//       $(target).children('.selected').removeClass('is-animated');
+//       $(target).children('.selected').removeClass('pulse');
+//     }, 1000);
+//
+//     starClicked = true;
+//   })
+//
+//   $('.half').click(function() {
+//     if (starClicked == true) {
+//       setHalfStarState(this)
+//     }
+//     $(this).closest('.rating').find('.js-score').text($(this).data('value'));
+//
+//     $(this).closest('.rating').data('vote', $(this).data('value'));
+//     calculateAverage()
+//     console.log(parseInt($(this).data('value')));
+//
+//   })
+//
+//   $('.full').click(function() {
+//     if (starClicked == true) {
+//       setFullStarState(this)
+//     }
+//     $(this).closest('.rating').find('.js-score').text($(this).data('value'));
+//
+//     $(this).find('js-average').text(parseInt($(this).data('value')));
+//
+//     $(this).closest('.rating').data('vote', $(this).data('value'));
+//     calculateAverage()
+//
+//     console.log(parseInt($(this).data('value')));
+//   })
+//
+//   $('.half').hover(function() {
+//     if (starClicked == false) {
+//       setHalfStarState(this)
+//     }
+//
+//   })
+//
+//   $('.full').hover(function() {
+//     if (starClicked == false) {
+//       setFullStarState(this)
+//     }
+//   })
+//
+// })
+//
+// function updateStarState(target) {
+//   $(target).parent().prevAll().addClass('animate');
+//   $(target).parent().prevAll().children().addClass('star-colour');
+//
+//   $(target).parent().nextAll().removeClass('animate');
+//   $(target).parent().nextAll().children().removeClass('star-colour');
+// }
+//
+// function setHalfStarState(target) {
+//   $(target).addClass('star-colour');
+//   $(target).siblings('.full').removeClass('star-colour');
+//   updateStarState(target)
+// }
+//
+// function setFullStarState(target) {
+//   $(target).addClass('star-colour');
+//   $(target).parent().addClass('animate');
+//   $(target).siblings('.half').addClass('star-colour');
+//
+//   updateStarState(target)
+// }
+//
+// function calculateAverage() {
+//   var average = 0
+//
+//   $('.rating').each(function() {
+//     average += $(this).data('vote')
+//   })
+//
+//   $('.js-average').text((average/ $('.rating').length).toFixed(1))
+// }
